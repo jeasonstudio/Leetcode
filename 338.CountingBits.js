f307c498f6e11a0ae89a24f2fbd8d329753f54d8
jeasonstudio <zjtong3576@sina.com>
2017-09-25 20:54:31 +0800

    finish 338

--- 338.CountingBits.js
+++ 338.CountingBits.js
@@ -3,14 +3,18 @@
  * @return {number[]}
  */
 var countBits = function(num) {
-  var res = []
-  for (var index = 0; index < num; index++) {
-    console.log(index.toString(2))
-    res[index] = index.toString(2).split('').reduce(function(a, b) {
-      return parseInt(a) + parseInt(b)
-    })
-  }
-  return res
+    var res = []
+    for (var index = 0; index <= num; index++) {
+        // console.log(index.toString(2))
+        var self = index.toString(2).split('')
+        res[index] = 0
+        self.forEach(function(item) {
+            res[index] += parseInt(item)
+        })
+    }
+    return res
 }
 
 console.log(countBits(5))
+
+// Your runtime beats 10.46 % of javascript submissions.
