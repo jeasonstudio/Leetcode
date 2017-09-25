7c232087ae1f1a21f59471331bd4c8600dec563b
jeasonstudio <zjtong3576@sina.com>
2017-09-25 20:43:34 +0800

    rank

--- 461.HammingDistance.js
+++ 461.HammingDistance.js
@@ -4,20 +4,22 @@
  * @return {number}
  */
 var hammingDistance = function(x, y) {
-  var tx = x.toString(2)
-  var ty = y.toString(2)
-  var length = tx.length > ty.length ? tx.length : ty.length
-  while (tx.length < length) {
-    tx = '0' + tx
-  }
-  while (ty.length < length) {
-    ty = '0' + ty
-  }
-  tx = tx.split("")
-  ty = ty.split("")
-  var res = 0
-  for (var index = 0; index < length; index++) {
-    if (tx[index] !== ty[index]) res += 1
-  }
-  return res
+    var tx = x.toString(2)
+    var ty = y.toString(2)
+    var length = tx.length > ty.length ? tx.length : ty.length
+    while (tx.length < length) {
+        tx = '0' + tx
+    }
+    while (ty.length < length) {
+        ty = '0' + ty
+    }
+    tx = tx.split("")
+    ty = ty.split("")
+    var res = 0
+    for (var index = 0; index < length; index++) {
+        if (tx[index] !== ty[index]) res += 1
+    }
+    return res
 }
+
+// Your runtime beats 67.57 % of javascript submissions.
