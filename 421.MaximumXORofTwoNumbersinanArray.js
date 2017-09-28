f665b36c91bbf2ad99794e489d45758e452d7023
Jeason <zhaojitong@xiaomi.com>
2017-09-28 14:02:19 +0800

    finish

--- 421.MaximumXORofTwoNumbersinanArray.js
+++ 421.MaximumXORofTwoNumbersinanArray.js
@@ -3,15 +3,25 @@
  * @return {number}
  */
 var findMaximumXOR = function(nums) {
-  var res = 0
-  for (var index = 0; index < nums.length; index++) {
-    for (var jndex = 0; jndex < nums.length; jndex++) {
-      if (nums[index] === nums[jndex]) continue
-      var self = nums[index] ^ nums[jndex]
-      res = self > res ? self : res
-    }
+  var max = mask = 0
+  for(var i = 31; i >= 0; i--){
+      mask = mask | (1 << i)
+      var set = []
+      for (var num = 0; num < nums.length; num++) {
+        var item = num & mask
+        if (set.indexOf(item) < 0) {
+          set.push(item)
+        }
+      }
+      var tmp = max | (1 << i)
+      for(var prefix = 0; prefix < set.length; prefix++){
+          if(set.indexOf(tmp ^ prefix) >= 0) {
+              max = tmp
+              break
+          }
+      }
   }
-  return res
+  return max
 }
 
 console.log(findMaximumXOR([3, 10, 5, 25, 2, 8]))
